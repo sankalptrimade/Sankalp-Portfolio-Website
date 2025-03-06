9c7bade9b50c68b5bb64616bcefd0c6b876407ba
sankalptrimade <sankalptrimade0014@gmail.com>
2025-03-06 20:59:05 +0530

    Completed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+import MobileMenu from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && (
@@ -13,8 +16,13 @@ function App() {
           }}
         />
       )}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
