e79309c53e3b721dd29237f76fb0218ca303e86a
sankalptrimade <sankalptrimade0014@gmail.com>
2025-03-06 23:57:49 +0530

    Completed Projects Page

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
                 {frontend.map((tech, key) => (
                   <span
                     key={key}
-                    className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition"
+                    className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
                   >
                     {tech}
                   </span>
